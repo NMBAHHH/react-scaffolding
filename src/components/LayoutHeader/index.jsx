813340e73c1d371e35cee71386639ae2361cefaf
xuya227939 <hezhiqianye@gmail.com>
2019-07-21 15:01:06 +0800

    添加用户

--- src/components/LayoutHeader/index.jsx
+++ src/components/LayoutHeader/index.jsx
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
+import { Avatar, Dropdown, Menu } from 'antd';
 import './index.less';
+
+const menu = (
+    <Menu>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href={false}>
+                退出
+            </a>
+        </Menu.Item>
+    </Menu>
+);
+
 class Index extends Component {
     constructor(props) {
         super(props);
@@ -8,9 +20,20 @@ class Index extends Component {
 
     render() {
         return (
-            <div className="header-name">
-                订单系统
-            </div>
+            <section className="header">
+                <div className="header-left">
+                    订单系统
+                </div>
+                <div className="header-right">
+                    <span className="message">消息</span>
+                    <Dropdown className="drop-down" overlay={menu}>
+                        <div>
+                            <Avatar className="avatar" size={48} icon="user" />
+                            <span className="name">Faker</span>
+                        </div>
+                    </Dropdown>
+                </div>
+            </section>
         );
     }
 }
