fc740c2292c836d0ab5f8157ee3daadd1ff8c1af
xuya227939 <hezhiqianye@gmail.com>
2019-07-22 11:08:27 +0800

    添加stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    'extends': 'stylelint-config-standard',
+    'rules': {
+        'at-rule-no-unknown': [ true, {
+            'ignoreAtRules': [
+                'extends',
+                'ignores'
+            ]
+        }],
+        'indentation': 4,
+        'number-leading-zero': null,
+        'unit-whitelist': ['em', 'rem', 's', 'px', 'deg', 'all'],
+        'no-eol-whitespace': [true, { 'ignore': 'empty-lines' }],
+        'selector-list-comma-newline-after': 'never-multi-line'
+    }
+};
