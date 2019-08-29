5da4fe672a15d5d526f9782686200d81ca77339c
xuya227939 <hezhiqianye@gmail.com>
2019-08-29 14:49:07 +0800

    [new] 保存全局正在操作的actionType

--- src/reducers/rootReducers.js
+++ src/reducers/rootReducers.js
@@ -1,7 +1,17 @@
 // reducers配置文件
-import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import orderList from './orderList';
+
+// 保存当前正在执行的action type
+const combineReducers = (reducers) => {
+    return (state = {}, action) => {
+        return Object.keys(reducers).reduce((nextState, key) => {
+            nextState[key] = reducers[key](state[key], action);
+            return nextState;
+        }, { actionType: action.type });
+    };
+};
+
 const rootReducers = combineReducers({
     orderList,
     router: routerReducer
