2b0a1fb12efb0a9a018cb31cd3ec0928a85d0348
xuya227939 <hezhiqianye@gmail.com>
2019-10-01 15:40:39 +0800

    [new] 更换CDN、更新16.8组件获取数据写法

--- src/actions/orderList.js
+++ src/actions/orderList.js
@@ -1,11 +1,10 @@
 import * as api from '../servers/table';
 
 // 获取表格数据
-export const GET_TABLE = 'GET_TABLE';
+export const GET_TABLE = 'getTable';
 export function getTable(params) {
     return {
         type: GET_TABLE,
-        payload: api.getTable(params),
-        isLoading: true
+        payload: api.getTable(params)
     };
 }
